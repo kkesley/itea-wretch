ba4e5fefee20b8c6ca1ca37e25f24ab9bbd21431
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-30 09:09:07 +1000

    index

--- src/platform/index.js
+++
@@ -1,7 +0,0 @@
-import login from './login'
-import refreshToken from './refresh-token'
-import register from './register'
-
-export const loginAPI = login
-export const refreshTokenAPI = refreshToken
-export const registerAPI = register
