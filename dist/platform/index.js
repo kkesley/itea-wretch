963b5e54b819bf9640474a1732f58831c076aef4
= <s3642807@student.rmit.edu.au>
2018-07-29 20:10:29 +1000

    add register api

--- dist/platform/index.js
+++ dist/platform/index.js
@@ -1,5 +1,7 @@
 import login from './login';
 import refreshToken from './refresh-token';
+import register from './register';
 
 export const loginAPI = login;
 export const refreshTokenAPI = refreshToken;
+export const registerAPI = register;
