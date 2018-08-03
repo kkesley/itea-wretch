143bd643eb043cfaf435f3b1393f8ee3dfd530c3
kkesley <kendrick.kesley.dev@gmail.com>
2018-08-03 23:25:41 +1000

    index

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 import wretch from 'wretch';
 import moment from 'moment-timezone';
 import { put } from 'redux-saga/effects';
+import storage from 'store';
 export const SERVICES = {
     PLATFORM: "PLATFORM"
 };
@@ -13,7 +14,7 @@ export const API = ({ auth } = { auth: null }) => {
     // Set the base url
     .url(BASE_URL)
     // Set headers
-    .headers({ "TZ": moment.tz.guess() })
+    .headers({ "TZ": moment.tz.guess() }).headers({ "Lang": storage.get("lang") || "id" })
     // Handle 500 errors
     .resolve(_ => _.internalError(err => ({ status: 500, body: err.message })))
     // Handle 502 errors
