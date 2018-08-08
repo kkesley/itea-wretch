093c9f251f45709968c11e948eba26e941e8b49b
kkesley <kendrick.kesley.dev@gmail.com>
2018-08-08 23:03:11 +1000

    test

--- src/index.js
+++ src/index.js
@@ -76,7 +76,8 @@ export function* callAPI({service, url, method, body, query, listener, listenCod
     }else if(method === "POST"){
         req = req.post(body)
     }
-    res = yield req.json(res => ({status: 200, body: res})).catch(err => ({status: err.status, body: err.message}))
+    res = yield req.json(res => ({status: 200, body: res}))
+    //.catch(err => ({status: err.status, body: err.message}))
     if(!listener && listenCode.indexOf(res.status) >= 0){
         return res
     }else{
